5b83dc6092ea435fd11b7aa6cd436b2c5185ffae
unknown <rahmanrakibur6589@gmail.com>
2021-05-29 01:14:29 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// Calculate Factorial of a number using for loop
+// function getFactorial(number) {
+//     var factorial = 1;
+//     for (let i = 1; i <= number; i++) {
+//         factorial = factorial * i;
+//     }
+//     return factorial;
+// }
+// var result = getFactorial(5);
+// console.log(result);
+
+
+
+// Calculate Factorial of a number using a while loop
+// function factorial(num) {
+//     var i = 1;
+//     var fac = 1;
+//     while (i <= num) {
+//         fac = fac * i;
+//         i++;
+//     }
+//     return fac;
+// }
+// var outPut = factorial(8);
+// console.log(outPut);
+
+
+// Calculate Factorial in a Recursive function
+// function getRecursiveFac(n) {
+//     if (n == 0) {
+//         return 1;
+//     }
+//     else {
+//         return n * getRecursiveFac(n - 1);
+//     }
+// }
