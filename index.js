889ad3b74c050e1cb7211537040ce85aeff6a53d
unknown <rahmanrakibur6589@gmail.com>
2021-05-29 02:01:37 -0700

    fibo complete

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-// Calculate Factorial of a number using for loop
-// function getFactorial(number) {
-//     var factorial = 1;
-//     for (let i = 1; i <= number; i++) {
-//         factorial = factorial * i;
-//     }
-//     return factorial;
-// }
-// var result = getFactorial(5);
-// console.log(result);
+// Create a Fibonacci Series using a for loop
+function getFibonacci(n) {
+    var fibo = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+    return fibo;
+}
+
+
+// Fibonacci Element in a Recursive Way
+function getFibonacci(n) {
+    if (n == 0) {
+        return 0;
+    }
+    else if (n == 1) {
+        return 1;
+    }
+    else {
+        return getFibonacci(n - 1) + getFibonacci(n - 2);
+    }
+}
+
+
+// Create Fibonacci series in a recursive way
+function getFiboSeries(n) {
+    if (n == 0) {
+        return [0];
+    }
+    else if (n == 1) {
+        return [0, 1];
+    }
+    else {
+        var fibo = getFiboSeries(n - 1);
+        var nextElement = fibo[n - 1] + fibo[n - 2];
+        fibo.push(nextElement);
+    }
+    return fibo;
+}
 
 
 
-// Calculate Factorial of a number using a while loop
-// function factorial(num) {
-//     var i = 1;
-//     var fac = 1;
-//     while (i <= num) {
-//         fac = fac * i;
-//         i++;
-//     }
-//     return fac;
-// }
-// var outPut = factorial(8);
-// console.log(outPut);
 
 
-// Calculate Factorial in a Recursive function
-// function getRecursiveFac(n) {
-//     if (n == 0) {
-//         return 1;
-//     }
-//     else {
-//         return n * getRecursiveFac(n - 1);
-//     }
-// }
