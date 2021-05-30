2f4cabd36c2665e2e83d905308b6e9b63a39abe2
unknown <rahmanrakibur6589@gmail.com>
2021-05-29 23:11:09 -0700

    count the number in a string

--- index.js
+++ index.js
@@ -40,3 +40,13 @@
 // }
 
 
+//Count the number of words in a string
+// var speech = "text";
+// var count = 0;
+// for (var i = 0; i < speech.length; i++) {
+//     var element = speech[i];
+//     if (element == ' ' && speech[i - 1] != ' ') {
+//         count++;
+//     }
+// }
+// count++;
