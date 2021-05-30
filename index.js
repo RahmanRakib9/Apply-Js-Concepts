af01723f8f3130527cca0e78c7867f3dfea2bdb8
unknown <rahmanrakibur6589@gmail.com>
2021-05-29 21:16:43 -0700

    reverse string

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 //Find the largest element of an array OR:
 //Find out the highest marks in your class on math
 
-function getBigger(marks) {
-    var max = 0;
-    for (let i = 0; i < marks.length; i++) {
-        const element = marks[i];
-        if (element > max) {
-            max = element;
-        }
-    }
-    return max;
-}
-var highestMarks = [55, 77, 88, 95, 87, 83];
-console.log(getBigger(highestMarks));
+// function getBigger(marks) {
+//     var max = 0;
+//     for (let i = 0; i < marks.length; i++) {
+//         const element = marks[i];
+//         if (element > max) {
+//             max = element;
+//         }
+//     }
+//     return max;
+// }
+// var highestMarks = [55, 77, 88, 95, 87, 83];
+// console.log(getBigger(highestMarks));
+
+//Sum of all numbers in an array
+// function getSum(num) {
+//     var sum = 0;
+//     for (var i = 0; i < num.length; i++) {
+//         const element = num[i];
+//         sum = sum + element;
+//     }
+//     return sum;
+// }
+
+
+
+
+//Reverse a string
+// function reverseString(string) {
+//     var reverse = '';
+//     for (var i = 0; i < string.length; i++) {
+//         var character = string[i];
+//         reverse = character + reverse;
+//     }
+//     return reverse;
+// }
+
