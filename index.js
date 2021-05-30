811070e09c5153f0f94a16c9a4db21562f01f24b
unknown <rahmanrakibur6589@gmail.com>
2021-05-29 20:58:48 -0700

    find largest element of an array

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-// Unit Convert Inch to Feet use variable and function
-function inchToFeet(inch){
-    var feet=inch/12;
-    return feet;
+//Find the largest element of an array OR:
+//Find out the highest marks in your class on math
+
+function getBigger(marks) {
+    var max = 0;
+    for (let i = 0; i < marks.length; i++) {
+        const element = marks[i];
+        if (element > max) {
+            max = element;
+        }
+    }
+    return max;
 }
-
-//Unit Convert kilometer to meter use variable and function
-function kilometerToMeter(kilometer){
-    var meter=kilometer*1000;
-    return meter;
-}
-
-
+var highestMarks = [55, 77, 88, 95, 87, 83];
+console.log(getBigger(highestMarks));
